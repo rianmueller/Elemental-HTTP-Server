5a0913b356b401487b97eb69c1371e2b7305cac2
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-19 18:58:15 -1000

    resolves server responses

--- server.js
+++ server.js
@@ -2,23 +2,20 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(function(request, response) {
-  //   fs.readdir("./public", function(error, files) {
-  //     console.log(files);
-  //   });
-
   // GET REQUESTS
+
   if (request.method === "GET") {
     console.log(request.method + " " + request.url);
-    // GET (invalid path)
+    // 404 when request is invalid
     if (!fs.existsSync("./public" + request.url)) {
       fs.readFile("./public/404.html", function(error, data) {
         if (error) {
-          console.log("REQUEST " + request.url + " ERROR");
+          console.log(request.url + " cannot be found");
         } else {
           response.writeHead(404, { "Content-type": "text/html" });
           response.write(data);
           response.end();
-          console.log(request.url + " does not exist");
+          console.log(request.url + " cannot be found, here's a 404 instead");
         }
       });
     } else {
@@ -26,7 +23,7 @@ const server = http.createServer(function(request, response) {
         // GET /
         fs.readFile("./public/index.html", function(error, data) {
           if (error) {
-            console.log("REQUEST " + request.url + " ERROR");
+            console.log(request.url + " cannot be found");
           } else {
             response.writeHead(200, { "Content-type": "text/html" });
             response.write(data);
@@ -37,7 +34,7 @@ const server = http.createServer(function(request, response) {
       } else if (request.url === "/css/styles.css") {
         fs.readFile("./public/css/styles.css", function(error, data) {
           if (error) {
-            console.log("REQUEST " + request.url + " ERROR");
+            console.log(request.url + " cannot be found");
           } else {
             response.writeHead(200, { "Content-type": "text/css" });
             response.write(data);
@@ -48,7 +45,7 @@ const server = http.createServer(function(request, response) {
       } else if (request.url) {
         fs.readFile("./public" + request.url, function(error, data) {
           if (error) {
-            console.log("REQUEST " + request.url + " ERROR");
+            console.log(request.url + " cannot be found");
           } else {
             response.writeHead(200, { "Content-type": "text/html" });
             response.write(data);
@@ -71,23 +68,34 @@ const server = http.createServer(function(request, response) {
         let keys = formDataArray[i].split("=");
         formDataObject[keys[0]] = keys[1];
       }
-      if (!formDataObject["elementName"]) {
-        console.log("Missing elementName");
+      // Return 500 if request is incomplete
+      if (
+        !formDataObject["elementName"] ||
+        !formDataObject["elementSymbol"] ||
+        !formDataObject["elementAtomicNumber"] ||
+        !formDataObject["elementDescription"]
+      ) {
+        console.log("Request is missing one or more values");
         request.on("end", function() {
           response.writeHead(500, { "Content-Type": "application/json" });
           let responseBody = {
-            error: "Missing elementName"
+            error: "Requeste is missing one or more values"
           };
           response.end(JSON.stringify(responseBody));
         });
-      } else {
-        if (
-          formDataObject["elementName"].toLowerCase() === "index" ||
-          formDataObject["elementName"].toLowerCase() === "404"
-        ) {
-          formDataObject["elementName"] = "invalidName";
-        }
-
+        // Return 500 if file name is index or 404
+      } else if (
+        formDataObject["elementName"] === "index" ||
+        formDataObject["elementName"] === "404"
+      ) {
+        console.log("Invalid elementName");
+        request.on("end", function() {
+          response.writeHead(500, { "Content-Type": "application/json" });
+          let responseBody = {
+            error: "Invalid elementName"
+          };
+          response.end(JSON.stringify(responseBody));
+        });
         // Create new HTML file and populate with POST data
         let newElement = `<!DOCTYPE html>
 <html lang="en">
@@ -115,16 +123,13 @@ const server = http.createServer(function(request, response) {
             console.log("File created successfully");
           }
         );
-
         // Update index.html
-
         fs.readFile("./public/index.html", function(error, data) {
           if (error) throw error;
 
-          // copy file to memory
+          // Add element to end of list
           let indexHTML = data.toString();
           if (indexHTML.search(formDataObject["elementName"]) === -1) {
-            // add element to end of list
             let addedElement = indexHTML.replace(
               "</ol>",
               `  <li>
@@ -134,8 +139,7 @@ const server = http.createServer(function(request, response) {
       </li>
     </ol>`
             );
-
-            // update the count of elements
+            // Update the count of elements
             let split = addedElement.split("\n");
             split.splice(10, 1);
             let join = split.join("\n").replace(
@@ -143,8 +147,7 @@ const server = http.createServer(function(request, response) {
               `</h2>
     <h3>There are ${addedElement.split("<li>").length - 1}</h3>`
             );
-
-            // write to index.html
+            // Write to index.html
             fs.writeFile("./public/index.html", join, function(error) {
               if (error) throw error;
               console.log("index.html updated successfully");
@@ -153,7 +156,7 @@ const server = http.createServer(function(request, response) {
             console.log("index.html already updated with element");
           }
         });
-
+        // Send success response
         request.on("end", function() {
           response.writeHead(200, { "Content-Type": "application/json" });
           const responseBody = { success: "true" };
@@ -163,22 +166,19 @@ const server = http.createServer(function(request, response) {
     });
   }
 
-  // PUT requests
+  // PUT REQUESTS
 
   if (request.method === "PUT") {
     request.on("data", function(data) {
       console.log(request.method + " " + request.url);
-
       // Parse the PUT data
       let formDataObject = {};
       let formDataArray = data.toString().split("&");
-
       for (let i in formDataArray) {
         let keys = formDataArray[i].split("=");
         formDataObject[keys[0]] = keys[1];
       }
-
-      // File must exist
+      // Return 500 if file does not exist
       if (!fs.existsSync("./public" + request.url)) {
         console.log("resource " + request.url + " does not exist");
         request.on("end", function() {
@@ -188,7 +188,7 @@ const server = http.createServer(function(request, response) {
           };
           response.end(JSON.stringify(responseBody));
         });
-        // File name must match element name
+        // Return 500 if file name does not match element name
       } else if (
         "./public" + request.url !==
         "./public/" + formDataObject["elementName"].toLowerCase() + ".html"
@@ -214,7 +214,7 @@ const server = http.createServer(function(request, response) {
           };
           response.end(JSON.stringify(responseBody));
         });
-        // All input fields must be populated
+        // Return 500 if request is incomplete
       } else if (
         !formDataObject["elementName"] ||
         !formDataObject["elementSymbol"] ||
@@ -229,7 +229,7 @@ const server = http.createServer(function(request, response) {
           };
           response.end(JSON.stringify(responseBody));
         });
-        // File name must not be index or 404
+        // Return 500 if file name is index or 404
       } else if (
         formDataObject["elementName"] === "index" ||
         formDataObject["elementName"] === "404"
@@ -270,7 +270,7 @@ const server = http.createServer(function(request, response) {
             console.log("File created successfully");
           }
         );
-
+        // Send success response
         request.on("end", function() {
           response.writeHead(200, { "Content-Type": "application/json" });
           let responseBody = { success: "true" };
@@ -280,46 +280,41 @@ const server = http.createServer(function(request, response) {
     });
   }
 
-  // DELETE requests
+  // DELETE REQUESTS
 
   if (request.method === "DELETE") {
     console.log(request.method + " " + request.url);
-
-    // File must exist
+    // Return 500 if file does not exist
     if (!fs.existsSync("./public" + request.url)) {
       console.log("resource " + request.url + " does not exist");
-      request.on("end", function() {
-        response.writeHead(500, { "Content-Type": "application/json" });
-        let responseBody = {
-          error: "resource " + request.url + " does not exist"
-        };
-        response.end(JSON.stringify(responseBody));
-      });
-      // File must be user-created
+      response.writeHead(500, { "Content-Type": "application/json" });
+      let responseBody = {
+        error: "resource " + request.url + " does not exist"
+      };
+      response.end(JSON.stringify(responseBody));
+      // Disallow deletion of essential files
     } else if (
       request.url === "/index.html" ||
       request.url === "/404.html" ||
-      request.url === "/css/styles.css"
+      request.url === "/css/styles.css" ||
+      request.url === "/hydrogen.html" ||
+      request.url === "/helium.html"
     ) {
       console.log("resource " + request.url + " cannot be deleted");
-      request.on("end", function() {
-        response.writeHead(500, { "Content-Type": "application/json" });
-        let responseBody = {
-          error: "resource " + request.url + " cannot be deleted"
-        };
-        response.end(JSON.stringify(responseBody));
-      });
+      response.writeHead(500, { "Content-Type": "application/json" });
+      let responseBody = {
+        error: "resource " + request.url + " cannot be deleted"
+      };
+      response.end(JSON.stringify(responseBody));
     } else {
-      // Delete file
+      // Delete element html file
       fs.unlinkSync("./public" + request.url);
-
+      console.log(request.url + " deleted successfully");
       // Update index.html
       fs.readFile("./public/index.html", function(error, data) {
         if (error) throw error;
-
-        // Copy index.html to memory
+        // Find the element to delete
         let indexHTML = data.toString();
-        // Find the element that matches the request.url
         let split = indexHTML.split("\n");
         for (let i = 0; i < split.length; i++) {
           if (split[i].includes(request.url) === true) {
@@ -327,40 +322,30 @@ const server = http.createServer(function(request, response) {
             split.splice(i - 1, 3);
           }
         }
-        // Count all remaining elements
+        // Count the remaining elements
         let count = 0;
         for (let i = 0; i < split.length; i++) {
           if (split[i].includes("<li>") === true) {
             count++;
           }
         }
-        // Replace <h3>
+        // Update <h3> element count
         for (let i = 0; i < split.length; i++) {
           if (split[i].includes("<h3>") === true) {
             split[i] = "    <h3>There are " + count + "</h3>";
           }
         }
-        // Rebuild index.html
+        // Write to index.html
         indexHTML = split.join("\n");
-        console.log(indexHTML);
         fs.writeFile("./public/index.html", indexHTML, function(error) {
           if (error) throw error;
           console.log("index.html updated successfully");
         });
       });
-
-      request.on("end", function() {
-        response.writeHead(200, { "Content-Type": "application/json" });
-        const responseBody = { success: "true" };
-        response.end(JSON.stringify(responseBody));
-      });
-
       // Send success response
-      request.on("end", function() {
-        response.writeHead(200, { "Content-Type": "application/json" });
-        let responseBody = { success: true };
-        response.end(JSON.stringify(responseBody));
-      });
+      response.writeHead(200, { "Content-Type": "application/json" });
+      let responseBody = { success: "true" };
+      response.end(JSON.stringify(responseBody));
     }
   }
 });
